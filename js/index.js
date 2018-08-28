c7338ec51382cbf415537f4d8ae6473e40d6875f
xianghaole <1335455344@qq.com>
2018-08-28 10:52:35 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(function(){
+    //alert($(document).width());
+    $('.bottom').width($('body').width()).css('left',-$('#nav .menu').offset().left);
+    $('#nav .menu').hover(function(){
+        $(this).children('.bottom').stop().animate({height:229},300).addClass('current');
+    },function(){
+        $(this).children('.bottom').stop().animate({height:0},300,function(){
+            $(this).removeClass('current');
+        });
+    });
+    //切换效果
+        $('#nav .menu .top a').mouseenter(function(){
+
+            var index = $(this).index();
+            $('#nav .menu .bottom ul li').eq(index).show().siblings('li').hide();
+        });
+    //小米闪购区切换效果
+    //$('#text ul li button').click(function(){
+    //    alert(1);
+    //    console.log(1);
+    //    //$('#m_show .mmdd_box ul').hide().siblings('ul').show();
+    //    var index = $(this).index();
+    //    $('#m_show .mmdd_box ul').eq(index).hide().siblings('ul').show();
+    //
+    //});
+
+
+
+
+});
